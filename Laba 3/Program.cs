fccc98975170197f41f9db66584b9dc0f68e7981
Олександр Клименко <alklimenko227@outlook.com>
2022-12-01 14:42:53 +0200

    near the end

--- Laba 3/Program.cs
+++ Laba 3/Program.cs
@@ -1,10 +1,34 @@
-﻿namespace Laba_3
+﻿using System.Reflection.Metadata.Ecma335;
+
+namespace Laba_3
 {
     internal class Program
     {
+        static double CalcFactorial (uint num)
+        {
+            if (num <= 1)
+            {
+                return 1;
+            }
+            return num * CalcFactorial(num - 1);
+        }
+        static double CalcExp (double x, int n = 0, double precision = 1e-10)
+        {
+          double current = Math.Pow(x, n) / CalcFactorial((uint)n);
+            if (current < precision )
+            {
+                return current;
+            }
+            return current + CalcExp(x, n + 1, precision);
+        }
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //uint Fact = (uint)CalcFactorial(Convert.ToUInt32(Console.ReadLine()));
+            //Console.WriteLine(Fact);
+            double x = Convert.ToDouble(Console.ReadLine());
+            double result = CalcExp(x);
+            Console.WriteLine("Exp(x)      = {0}", result);
+            Console.WriteLine("Math.Exp(x) = {0}", Math.Exp(x));
         }
     }
 }
